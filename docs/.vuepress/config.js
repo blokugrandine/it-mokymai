c8d4f11ab9f63a0ac5caacedae6e10c61a99d7f6
Dovydas Putrius <putrius.dovydas@gmail.com>
2024-08-21 21:43:57 +0300

    add deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'IT Mokymai',
-  description:
-    'IT Mokymų platforma pradedantiesiems',
+  description: 'IT Mokymų platforma pradedantiesiems',
+  base: '/it-mokymai/',
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
